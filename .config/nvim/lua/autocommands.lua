5f365f597484a24a558f6fac9fcbfc556fd6f32f
Gabriel Moreno <gantoreno@gmail.com>
2021-07-01 18:19:19 -0400

    Update autocommands for JSX & TSX

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -9,8 +9,8 @@
 local command = vim.api.nvim_command
 
 command('autocmd BufEnter * if &filetype == \'NvimTree\' | setlocal signcolumn=no | endif')
-command('autocmd BufNewFile,BufRead *.js,*.jsx set filetype=javascript.jsx')
-command('autocmd BufNewFile,BufRead *.tsx,*.ts set filetype=typescript.tsx')
+command('autocmd BufNewFile,BufRead *.js,*.jsx set filetype=javascriptreact')
+command('autocmd BufNewFile,BufRead *.tsx,*.ts set filetype=typescriptreact')
 command('autocmd BufNewFile,BufRead *.zsh-theme set filetype=zsh')
 command('autocmd BufNewFile,BufRead *.html,*.css,*.js,*.jsx,*.ts,*.tsx let g:user_emmet_expandabbr_key="<Tab>" | imap <expr> <Tab> emmet#expandAbbrIntelligent("<Tab>")')
 
