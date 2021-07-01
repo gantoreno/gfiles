0f3fbb49dc0ac13e02b2530ca91613554da217d4
Gabriel Moreno <gantoreno@gmail.com>
2021-07-01 12:35:33 -0400

    Add `vim-airline`

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,12 +1,16 @@
-local execute = vim.api.nvim_command
+local g = vim.g
 local fn = vim.fn
+local command = vim.api.nvim_command
 
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+  command 'packadd packer.nvim'
 end
+
+g.airline_theme = 'startrail'
+g.airline_powerline_fonts = true
 
 return require('packer').startup(function()
 	-- Packer can manage itself
@@ -14,4 +18,6 @@ return require('packer').startup(function()
 
 	use {'gantoreno/vim-startrail', as = 'startrail'}
 	use {'kyazdani42/nvim-tree.lua', as = 'nvim-tree'}
+	use {'vim-airline/vim-airline', as = 'airline'}
 end)
+
