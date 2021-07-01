2bd4d5b452787d26c9a7f6a6475d73041e229d83
Gabriel Moreno <gantoreno@gmail.com>
2021-07-01 16:48:08 -0400

    Update LSP colors

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -1,6 +1,7 @@
 local g = vim.g
 local o = vim.o
 local command = vim.api.nvim_command
+local highlight = vim.highlight
 
 g.startrail_italic_comments = false
 g.startrail_italic_functions = false
@@ -8,3 +9,8 @@ g.startrail_italic_functions = false
 command('colorscheme startrail')
 
 o.termguicolors = true
+
+command('au VimEnter * hi LspDiagnosticsDefaultError guifg=#cf8164')
+command('au VimEnter * hi LspDiagnosticsDefaultWarning guifg=#d2b45f')
+command('au VimEnter * hi LspDiagnosticsDefaultInformation guifg=#d2b45f')
+command('au VimEnter * hi LspDiagnosticsDefaultHint guifg=#7dc2c7')
