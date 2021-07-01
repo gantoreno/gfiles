43302ef54762bbe382fa69def5ec7ac2908ab811
Gabriel Moreno <gantoreno@gmail.com>
2021-07-01 17:47:42 -0400

    Update `keymaps.lua`

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -5,10 +5,10 @@ set_keymap('n', '<Space>', '<NOP>', {noremap = true, silent = true})
 
 g.mapleader = ' '
 
-set_keymap('n', '<Leader>cc', ':TComment<CR>', {noremap = true, silent = true})
-set_keymap('n', '<Leader>nn', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
-set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', {noremap = true, silent = true})
+set_keymap('', '<Leader>cc', ':TComment<CR>', {noremap = true, silent = true})
+set_keymap('', '<Leader>nn', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+set_keymap('', '<Leader>ff', ':Telescope find_files<CR>', {noremap = true, silent = true})
 
-set_keymap('n', '<Leader>wsus', ':!workspace up -s<CR><CR>', {silent = true})
-set_keymap('n', '<Leader>wsud', ':!workspace up -d<CR><CR>', {silent = true})
-set_keymap('n', '<Leader>wsue', ':!workspace up -e<CR><CR>', {silent = true})
+set_keymap('', '<Leader>wsus', ':!workspace up -s<CR><CR>', {silent = true})
+set_keymap('', '<Leader>wsud', ':!workspace up -d<CR><CR>', {silent = true})
+set_keymap('', '<Leader>wsue', ':!workspace up -e<CR><CR>', {silent = true})
