8d41ea946edf15d6f26c3a42fda1deb13c6bef50
Gabriel Moreno <gantoreno@gmail.com>
2021-07-02 09:24:14 -0400

    Create `ftplugin` & add plugins

---
+++ .config/nvim/ftplugin/cpp.lua
@@ -0,0 +1,18 @@
+--    ______      __         _      __   Gabriel Moreno
+--   / ____/___ _/ /_  _____(_)__  / /   ==============
+--  / / __/ __ `/ __ \/ ___/ / _ \/ /    E-mail:   gantoreno@gmail.com
+-- / /_/ / /_/ / /_/ / /  / /  __/ /     Website:  https://gantoreno.com
+-- \____/\__,_/_.___/_/  /_/\___/_/      GitHub:   https://github.com/gantoreno
+-- 
+-- C++ filetype plugin file
+
+-- Variables {{{
+local o = vim.o
+-- }}}
+
+-- Options {{{
+o.tabstop = 4
+o.shiftwidth = 4
+
+o.expandtab = true
+-- }}}
