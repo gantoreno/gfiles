3fa83a7c5c0b7d0e1d0825ba0bba9f496d80b29c
Gabriel Moreno <gantoreno@gmail.com>
2021-07-02 11:48:39 -0400

    Update LSP icons

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -118,10 +118,10 @@ return require('packer').startup(function()
     'glepnir/lspsaga.nvim',
     config = function()
       require'lspsaga'.init_lsp_saga {
-        error_sign = '',
-        warn_sign = '',
-        hint_sign = '',
-        infor_sign = '',
+        error_sign = '',
+        warn_sign = '',
+        hint_sign = '',
+        infor_sign = '',
       }
     end
   }
