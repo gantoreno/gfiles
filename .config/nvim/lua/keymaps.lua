3aecfb6ec1edaeb3203caf07fcbf108b4afe0984
Gabriel Moreno <gantoreno@gmail.com>
2021-07-09 22:56:02 -0400

    Update `workspace` keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -24,8 +24,8 @@ set_keymap("", "<leader>hh", ":ColorizerToggle<cr>", { noremap = true, silent = 
 set_keymap("", "<leader>pp", ":MarkdownPreview<cr>", { noremap = true, silent = true })
 set_keymap("", "<leader>ff", ":Telescope find_files<cr>", { noremap = true, silent = true })
 
-set_keymap("", "<leader>wsus", ":!workspace up -s<cr><cr>", { silent = true })
-set_keymap("", "<leader>wsud", ":!workspace up -d<cr><cr>", { silent = true })
-set_keymap("", "<leader>wsue", ":!workspace up -e<cr><cr>", { silent = true })
+set_keymap("", "<leader>wsus", ":!workspace up --single<cr><cr>", { silent = true })
+set_keymap("", "<leader>wsud", ":!workspace up --double<cr><cr>", { silent = true })
+set_keymap("", "<leader>wsut", ":!workspace up --triple<cr><cr>", { silent = true })
 set_keymap("", "<leader>wsd", ":!workspace down<cr><cr>", { silent = true })
 -- }}}
