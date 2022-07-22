3e72e183acf4d0e94c7f6f30c79bf88fbe165bfe
Gabriel Moreno <gantoreno@gmail.com>
2022-07-22 12:49:38 -0400

    Updates

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,17 +2,20 @@ local g = vim.g
 local o = vim.o
 local wo = vim.wo
 
-g.noshowmode = true
-
 o.number = true
+o.showmode = false
 o.expandtab = true
 o.cursorline = true
+o.splitright = true
 o.termguicolors = true
+
 o.cmdheight = 1
 o.scrolloff = 10
 o.shiftwidth = 2
 o.laststatus = 3
+
 o.mouse = "a"
+o.listchars = "eol:↴,nbsp:·,space:·,trail:·"
 o.background = "dark"
 o.clipboard = "unnamedplus"
 
