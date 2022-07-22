d05c45ac15001e39966e058e35617d4fda58f0d7
Gabriel Moreno <gantoreno@gmail.com>
2022-07-22 13:03:19 -0400

    Updates

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -66,6 +66,11 @@ return packer.startup(function(use)
           file_ignore_patterns = { "node_modules", ".git" },
         },
       })
+
+      vim.api.nvim_command("hi TelescopeBorder guifg=#61afef")
+      vim.api.nvim_command("hi TelescopePromptBorder guifg=#61afef")
+      vim.api.nvim_command("hi TelescopePreviewBorder guifg=#61afef")
+      vim.api.nvim_command("hi TelescopeResultsBorder guifg=#61afef")
     end,
   })
 
