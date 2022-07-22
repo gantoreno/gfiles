6cdba89472e93db73cf696476c2be9b7d5f4e003
Gabriel Moreno <gantoreno@gmail.com>
2022-07-22 13:02:24 -0400

    Updates

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -4,8 +4,6 @@ command("autocmd BufNewFile,BufRead *.zsh-theme set filetype=zsh")
 command("autocmd BufNewFile,BufRead *.js,*.jsx, set filetype=javascriptreact")
 command("autocmd BufNewFile,BufRead *.ts,*.tsx, set filetype=typescriptreact")
 
-command(
-  "autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx,*.lua,*.py,*.c,*.cpp,*.h,*.hpp,CMakeLists.txt lua vim.lsp.buf.formatting_sync()"
-)
+command("autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx,*.lua,*.py,*.c,*.cpp,*.h,*.hpp,CMakeLists.txt lua vim.lsp.buf.formatting_sync()")
 
 command("autocmd FileType floaterm setlocal nonu nornu nocursorline signcolumn=no")
--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -6,3 +6,8 @@ local config = {
 
 onedark.setup(config)
 onedark.load()
+
+vim.api.nvim_command("hi TelescopeBorder guifg=#61afef")
+vim.api.nvim_command("hi TelescopePromptBorder guifg=#61afef")
+vim.api.nvim_command("hi TelescopePreviewBorder guifg=#61afef")
+vim.api.nvim_command("hi TelescopeResultsBorder guifg=#61afef")
