fa844f74eeef6fe3a37fcbea239495d47216f47f
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-06-28 15:15:58 -0400

    Setup automatic packer bootstrap

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,24 @@
 local g = vim.g
 
+local fn = vim.fn
+local cmd = vim.cmd
 local command = vim.api.nvim_command
+
+local ensure_packer = function()
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+
+    cmd('packadd packer.nvim')
+
+    return true
+  end
+
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 -- Configure packer
 command("packadd packer.nvim")
@@ -25,6 +43,10 @@ require('packer').startup(function(use)
   use({ 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } })
 
   use({ 'nvim-treesitter/nvim-treesitter', requires = 'nvim-treesitter/playground', run = ':TSUpdate' })
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
 -- Plugin configuration
