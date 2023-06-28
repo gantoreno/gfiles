ed3e063690f9e183a0fde02bdf9781f18b38a3ac
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-06-28 15:47:42 -0400

    Update plugins and keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -8,8 +8,8 @@ function map(mode, lhs, rhs, opts)
   if opts then
     options = tbl_extend("force", options, opts)
   end
-  
-  api.nvim_set_keymap(mode, lhs, rhs, options) 
+
+  api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- Mappings
@@ -26,8 +26,8 @@ map("t", "<C-l>", "<C-\\><C-n><C-w>l", { silent = true })
 map("n", "<leader><left>", ":tabprev<CR>", { silent = true })
 map("n", "<leader><right>", ":tabnext<CR>", { silent = true })
 
-map("n", "<C-f>", ":Telescope find_files<CR>", { silent = true })
-map("n", "<C-g>", ":Telescope live_grep<CR>", { silent = true })
+map("n", "<C-f>", ":lua require('telescope.builtin').find_files({ hidden = true })<CR>", { silent = true })
+map("n", "<C-g>", ":lua require('telescope.builtin').live_grep({ hidden = true })<CR>", { silent = true })
 
 map("n", "<C-n>", ":NvimTreeToggle<CR>", { silent = true })
 
@@ -36,5 +36,5 @@ map("v", "<C-/>", ":CommentToggle<CR>", { silent = true })
 
 map("n", "<leadet>tsp", ":TSPlaygroundToggle<CR>", { silent = true })
 
-map("n", "<leader>lzg", ":FloatermNew --wintype=float --width=0.8 --height=0.8 --position=center lazygit<CR>", { silent = true })
-
+map("n", "<leader>lzg", ":FloatermNew --wintype=float --width=0.8 --height=0.8 --position=center lazygit<CR>",
+  { silent = true })
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,5 +1,3 @@
-local g = vim.g
-
 local fn = vim.fn
 local cmd = vim.cmd
 local command = vim.api.nvim_command
@@ -24,23 +22,119 @@ local packer_bootstrap = ensure_packer()
 command("packadd packer.nvim")
 
 require('packer').startup(function(use)
-  use('wbthomason/packer.nvim')
-
   use('/Users/gabrielmoreno/Developer/Personal/vim-gabriel')
 
-  use('windwp/nvim-autopairs')
-  use('terrortylor/nvim-comment')
+  use({
+    'wbthomason/packer.nvim',
+    config = function()
+      require("nvim-autopairs").setup({ check_ts = true })
+    end
+  })
 
-  use('nvim-tree/nvim-tree.lua')
-  use({ 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' })
+  use('windwp/nvim-autopairs')
+  use({
+    'terrortylor/nvim-comment',
+    config = function()
+      require("nvim_comment").setup({})
+    end
+  })
+
+  use({
+    'nvim-tree/nvim-tree.lua',
+    config = function()
+      require('nvim-tree').setup({
+        actions = {
+          open_file = { quit_on_open = true }
+        },
+        renderer = {
+          group_empty = true,
+          icons = {
+            git_placement = 'after',
+            glyphs = {
+              folder = {
+                default = '',
+                open = '',
+                arrow_closed = '',
+                arrow_open = '',
+              }
+            },
+          },
+          special_files = {}
+        },
+        update_focused_file = { enable = true },
+        tab = {
+          sync = {
+            open = true,
+            close = true,
+            ignore = {},
+          },
+        },
+        view = { signcolumn = "no" },
+        filters = { dotfiles = false },
+      })
+    end
+  })
+
+  use({
+    'nvim-telescope/telescope.nvim',
+    requires = 'nvim-lua/plenary.nvim',
+    config = function()
+      require('telescope').setup({
+        defaults = {
+          mappings = {
+            i = { ['<esc>'] = require('telescope.actions').close }
+          },
+          file_ignore_patterns = { 'node_modules', '.git', 'build', 'static' },
+        },
+        pickers = {
+          find_files = {
+            theme = "dropdown",
+          },
+          live_grep = {
+            theme = "dropdown",
+          }
+        },
+      })
+    end
+  })
 
   use('mhinz/vim-signify')
-  use('nvim-tree/nvim-web-devicons')
+  use({
+    'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('nvim-web-devicons').setup({
+        strict = true,
+        override_by_extension = {
+          ["astro"] = {
+            icon = "󰑣",
+            color = "#e0622a",
+            name = "Astro"
+          }
+        },
+      })
+    end
+  })
 
   use('voldikss/vim-floaterm')
 
-  use('neovim/nvim-lspconfig')
-  use({ 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } })
+  use({
+    'neovim/nvim-lspconfig',
+    config = function()
+      require('lspconfig.ui.windows').default_options.border = 'rounded'
+    end
+  })
+
+  use({
+    'hrsh7th/nvim-cmp',
+    requires = 'hrsh7th/cmp-nvim-lsp',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = 'all',
+        indent = { enable = true },
+        highlight = { enable = true },
+      })
+    end
+  })
 
   use({ 'nvim-treesitter/nvim-treesitter', requires = 'nvim-treesitter/playground', run = ':TSUpdate' })
 
@@ -50,65 +144,3 @@ require('packer').startup(function(use)
 end)
 
 -- Plugin configuration
-require("nvim-autopairs").setup({ check_ts = true })
-
-require("nvim_comment").setup({})
-
-require('nvim-tree').setup({
-  actions = {
-    open_file = { quit_on_open = true }
-  },
-  renderer = {
-    group_empty = true,
-    icons = {
-      git_placement = 'after',
-      glyphs = {
-        folder = {
-          default = '',
-          open = '',
-          arrow_closed = '',
-          arrow_open = '',
-        }
-      },
-    },
-    special_files = {}
-  },
-  update_focused_file = { enable = true },
-  tab = {
-    sync = {
-      open = true,
-      close = true,
-      ignore = {},
-    },
-  },
-  view = { signcolumn = "no" },
-  filters = { dotfiles = false },
-})
-
-require('telescope').setup({
-  defaults = {
-    mappings = {
-      i = { ['<esc>'] = require('telescope.actions').close }
-    },
-    file_ignore_patterns = { 'node_modules', '.git', 'build' },
-  },
-})
-
-require('lspconfig.ui.windows').default_options.border = 'rounded'
-
-require('nvim-treesitter.configs').setup({
-  ensure_installed = 'all',
-  indent = { enable = true },
-  highlight = { enable = true },
-})
-
-require('nvim-web-devicons').setup({
-  strict = true,
-  override_by_extension = {
-    ["astro"] = {
-      icon = "󰑣",
-      color = "#e0622a",
-      name = "Astro"
-    }
-  },
-})
