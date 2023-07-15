8fcd5258c965ab050887fbbb6c3f50bd37b35d99
Gabriel Moreno <gantoreno@gmail.com>
2023-07-14 23:56:54 -0400

    Update telescope.lua

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,6 +3,14 @@ require('telescope').setup({
     prompt_prefix = ' ',
     selection_caret = ' ',
     entry_prefix = ' ',
+    preview = false,
+    layout_strategy = 'vertical',
+    layout_config = {
+      vertical = {
+        prompt_position = 'top',
+        width = 0.5,
+      },
+    },
     mappings = {
       i = {
         ['<esc>'] = require('telescope.actions').close,
