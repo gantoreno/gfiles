cc2d9226a0d47178b4eee8ff738e98ad2d43a858
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-07-17 12:31:38 -0400

    Update floaterm.lua

--- .config/nvim/after/plugin/floaterm.lua
+++ .config/nvim/after/plugin/floaterm.lua
@@ -2,7 +2,7 @@ local g = vim.g
 
 g.floaterm_borderchars = '─│─│╭╮╯╰'
 g.floaterm_shell = 'NEOVIM=1 /usr/bin/env zsh'
-g.floaterm_height = 0.3
+g.floaterm_height = 0.25
 g.floaterm_autoclose = 2
 g.floaterm_wintype = 'split'
 g.floaterm_position = 'botright'
