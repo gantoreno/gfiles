0f46e598d53f1ba5b37c1d36889a2811af78138b
Gabriel Moreno <gantoreno@gmail.com>
2024-07-07 13:46:31 -0400

    Update lualine theme

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,1 +1,41 @@
-require('lualine').setup({})
+local colors = require "material.colors"
+
+local m = colors.main
+local e = colors.editor
+
+e.accent = m.blue
+
+local theme = {
+  normal = {
+    a = { fg = e.bg, bg = e.accent, gui = "bold" },
+    b = { fg = e.title, bg = e.selection },
+    c = { fg = e.fg, bg = e.bg_alt },
+  },
+  insert = {
+    a = { fg = e.bg, bg = m.green, gui = "bold" },
+    b = { fg = e.title, bg = e.selection },
+  },
+  visual = {
+    a = { fg = e.bg, bg = m.purple, gui = "bold" },
+    b = { fg = e.title, bg = e.selection },
+  },
+  replace = {
+    a = { fg = e.bg, bg = m.red, gui = "bold" },
+    b = { fg = e.title, bg = e.selection },
+  },
+  command = {
+    a = { fg = e.bg, bg = m.yellow, gui = "bold" },
+    b = { fg = e.title, bg = e.selection },
+  },
+  inactive = {
+    a = { fg = e.disabled, bg = e.bg },
+    b = { fg = e.disabled, bg = e.bg },
+    c = { fg = e.disabled, bg = e.bg }
+  }
+}
+
+require('lualine').setup({
+  options = {
+    theme = theme
+  }
+})
