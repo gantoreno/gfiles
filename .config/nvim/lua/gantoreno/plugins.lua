e7c5bf5cd7c636519c091cb5f483a1feda5d9c3f
Gabriel Moreno <gantoreno@gmail.com>
2024-07-07 12:17:44 -0400

    Add indent-blankline

--- .config/nvim/lua/gantoreno/plugins.lua
+++ .config/nvim/lua/gantoreno/plugins.lua
@@ -54,6 +54,7 @@ return packer.startup(function(use)
     'nvim-telescope/telescope.nvim',
     requires = 'nvim-lua/plenary.nvim',
   })
+  use('lukas-reineke/indent-blankline.nvim')
 
   -- Symbols
   use('nvim-tree/nvim-web-devicons')
