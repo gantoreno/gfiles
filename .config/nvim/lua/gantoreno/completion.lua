24c4b51fd0c6e6f97b620bcff90afe7cdddd9ca8
Gabriel Moreno <gantoreno@gmail.com>
2024-07-07 13:04:28 -0400

    Remove icon configuration for cmp

--- .config/nvim/lua/gantoreno/completion.lua
+++ .config/nvim/lua/gantoreno/completion.lua
@@ -12,38 +12,6 @@ cmp.setup({
       luasnip.lsp_expand(args.body)
     end,
   },
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol',
-      symbol_map = {
-        Class = '󰆩',
-        Color = '󰌁',
-        Constant = '󰭸',
-        Constructor = '󰆧',
-        Enum = '󰆜',
-        EnumMember = '󰆜',
-        Event = '',
-        Field = '󰆧',
-        File = '󰈤',
-        Folder = '󰉖',
-        Function = '󰆧',
-        Interface = '',
-        Key = '󰷖',
-        Method = '󰆧',
-        Module = '',
-        Operator = '󱖦',
-        Property = '󰯠',
-        Reference = '󱞫',
-        Snippet = '󰅴',
-        Struct = '󰌗',
-        Text = '󰭳',
-        TypeParameter = '󱄽',
-        Unit = '󱍓',
-        Value = '󰎠',
-        Variable = '󰮄',
-      },
-    }),
-  },
   completion = {
     completeopt = 'menu,menuone,noinsert',
   },
@@ -55,7 +23,6 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-
     -- Interaction
     ['<Cr>'] = cmp.mapping.confirm({ select = true }),
     ['<Tab>'] = cmp.mapping(function(fallback)
@@ -69,15 +36,5 @@ cmp.setup({
         fallback()
       end
     end, { 'i', 's' }),
-  },
-  window = {
-    completion = cmp.config.window.bordered({
-      winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None',
-      scrollbar = false,
-    }),
-    documentation = cmp.config.window.bordered({
-      winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None',
-      scrollbar = false,
-    }),
   },
 })
