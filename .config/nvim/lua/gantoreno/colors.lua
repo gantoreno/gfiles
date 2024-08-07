00f4ff2b289cfa5ec1cd7a64048402b1a6c6d67f
Gabriel Moreno <gantoreno@gmail.com>
2024-08-07 15:23:02 -0500

    Update colors

--- .config/nvim/lua/gantoreno/colors.lua
+++ .config/nvim/lua/gantoreno/colors.lua
@@ -9,7 +9,7 @@ require('onedark').setup({
     TabLineSel = { fg = '#9d9fa5', bg = '#38383d' },
     ['@tag.delimiter'] = { fg = '#9d9fa5' },
     ['@tag.attribute'] = { fg = '#D19A66' },
-    ['@tag'] = { fg = '#e86671' },
+    ['@tag'] = { fg = '#dcb672' },
   },
   style = 'warmer',
 })
