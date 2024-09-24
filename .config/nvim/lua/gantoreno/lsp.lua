23883c3a189da2258d624d635ab9d5e639c41b71
Gabriel Moreno <gantoreno@gmail.com>
2024-09-24 12:09:41 -0400

    Open diagnostics on semicolon

--- .config/nvim/lua/gantoreno/lsp.lua
+++ .config/nvim/lua/gantoreno/lsp.lua
@@ -4,75 +4,88 @@ local keymap = vim.keymap
 local lsp = vim.lsp
 local diagnostic = vim.diagnostic
 
-local cmp_lsp = require('cmp_nvim_lsp')
-local lspconfig = require('lspconfig')
+local cmp_lsp = require("cmp_nvim_lsp")
+local lspconfig = require("lspconfig")
 
-local navic = require('nvim-navic')
+local navic = require("nvim-navic")
 
 -- Setup language servers.
 local servers = {
-  'astro',
-  'clangd',
-  'eslint',
-  'elixirls',
-  'gopls',
-  'jsonls',
-  'lua_ls',
-  'rust_analyzer',
-  'templ',
-  'ts_ls',
+	"astro",
+	"clangd",
+	"eslint",
+	"elixirls",
+	"gopls",
+	"jsonls",
+	"lua_ls",
+	"rust_analyzer",
+	"templ",
+	"ts_ls",
 }
 
 for _, server in ipairs(servers) do
-  lspconfig[server].setup({
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { 'vim' },
-        },
-      },
-      document_formatting = false,
-    },
-    on_attach = function(client, bufnr)
-      if client.server_capabilities.documentSymbolProvider then
-        navic.attach(client, bufnr)
-      end
+	lspconfig[server].setup({
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { "vim" },
+				},
+			},
+			document_formatting = false,
+		},
+		on_attach = function(client, bufnr)
+			if client.server_capabilities.documentSymbolProvider then
+				navic.attach(client, bufnr)
+			end
 
-      client.capabilities = cmp_lsp.default_capabilities()
-    end,
-  })
+			client.capabilities = cmp_lsp.default_capabilities()
+		end,
+	})
 end
 
-lspconfig['elixirls'].setup({
-  cmd = { '/opt/homebrew/bin/elixir-ls' },
+lspconfig["elixirls"].setup({
+	cmd = { "/opt/homebrew/bin/elixir-ls" },
 })
 
-api.nvim_create_autocmd('LspAttach', {
-  group = api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(evt)
-    local opts = { buffer = evt.buf }
+api.nvim_create_autocmd("LspAttach", {
+	group = api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(evt)
+		local opts = { buffer = evt.buf }
 
-    keymap.set('n', 'K', lsp.buf.hover, opts)
-    keymap.set('n', 'r', lsp.buf.rename, opts)
-    keymap.set('n', '.', lsp.buf.code_action, opts)
-    keymap.set('n', 'gd', lsp.buf.definition, opts)
-    keymap.set('n', 'gD', lsp.buf.declaration, opts)
-  end,
+		keymap.set("n", "K", lsp.buf.hover, opts)
+		keymap.set("n", "r", lsp.buf.rename, opts)
+		keymap.set("n", ".", lsp.buf.code_action, opts)
+		keymap.set("n", "gd", lsp.buf.definition, opts)
+		keymap.set("n", "gD", lsp.buf.declaration, opts)
+		keymap.set("n", ";", function()
+			vim.diagnostic.open_float(0, {
+				scope = "cursor",
+				focusable = false,
+				close_events = {
+					"CursorMoved",
+					"CursorMovedI",
+					"BufHidden",
+					"InsertCharPre",
+					"WinLeave",
+				},
+			})
+		end, opts)
+	end,
 })
 
 -- Icons
 diagnostic.config({})
 
 local signs = {
-  Error = '',
-  Warn = '',
-  Hint = '',
-  Info = '',
+	Error = "",
+	Warn = "",
+	Hint = "",
+	Info = "",
 }
 
 for type, text in pairs(signs) do
-  local texthl = 'DiagnosticSign' .. type
-  local linehl = 'DiagnosticSign' .. type .. 'Line'
+	local texthl = "DiagnosticSign" .. type
+	local linehl = "DiagnosticSign" .. type .. "Line"
 
-  fn.sign_define(texthl, { text = text, texthl = texthl, linehl = linehl })
+	fn.sign_define(texthl, { text = text, texthl = texthl, linehl = linehl })
 end
