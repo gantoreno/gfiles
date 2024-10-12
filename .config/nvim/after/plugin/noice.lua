321c82991e4088fed84a150e8b3da8957b65143d
Gabriel Moreno <gantoreno@gmail.com>
2024-10-12 18:00:18 -0400

    Suppress noice messages

--- .config/nvim/after/plugin/noice.lua
+++ .config/nvim/after/plugin/noice.lua
@@ -12,4 +12,13 @@ require('noice').setup({
       },
     },
   },
+  routes = {
+    {
+      filter = {
+        event = 'notify',
+        find = 'No information available',
+      },
+      opts = { skip = true },
+    },
+  },
 })
