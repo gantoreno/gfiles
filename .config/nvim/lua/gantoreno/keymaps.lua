f1b03ec77c86af128a433b23686d083cf7409bd1
Gabriel Moreno <gantoreno@gmail.com>
2024-10-13 19:03:01 -0400

    Customize keymaps

--- .config/nvim/lua/gantoreno/keymaps.lua
+++ .config/nvim/lua/gantoreno/keymaps.lua
@@ -1,51 +1,111 @@
 local which_key = require('which-key')
-
 -- Generic
 which_key.add({
   -- Tabs
   { '<leader><left>', '<cmd>tabprev<cr>', desc = 'Previous tab' },
   { '<leader><right>', '<cmd>tabnext<cr>', desc = 'Next tab' },
-
   -- Lazygit
   {
     '<leader>lzg',
     '<cmd>FloatermNew --wintype=float --width=0.8 --height=0.8 --position=center lazygit<cr>',
     desc = 'Lazygit',
+    icon = { icon = '', color = 'cyan' },
   },
 })
-
 -- Find
 which_key.add({
   { '<leader>f', group = 'Find' },
-  { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Files', mode = 'n' },
-  { '<leader>ft', '<cmd>Telescope live_grep<cr>', desc = 'Text', mode = 'n' },
-  { '<leader>fb', '<cmd>Telescope git_branches<cr>', desc = 'Branches', mode = 'n' },
+  {
+    '<leader>ff',
+    '<cmd>Telescope find_files<cr>',
+    desc = 'Files',
+    icon = { icon = '', color = 'blue' },
+    mode = 'n',
+  },
+  {
+    '<leader>ft',
+    '<cmd>Telescope live_grep<cr>',
+    desc = 'Text',
+    icon = { icon = '', color = 'yellow' },
+    mode = 'n',
+  },
+  {
+    '<leader>fb',
+    '<cmd>Telescope git_branches<cr>',
+    desc = 'Branches',
+    icon = { icon = '', color = 'cyan' },
+    mode = 'n',
+  },
 })
-
 -- Goto
 which_key.add({
-  { '<leader>g', group = 'Goto' },
-  { '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>', desc = 'Definition', mode = 'n' },
-  { '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', desc = 'Declaration', mode = 'n' },
+  { '<leader>g', group = 'Goto', icon = { icon = '', color = 'azure' } },
+  {
+    '<leader>gd',
+    '<cmd>lua vim.lsp.buf.definition()<cr>',
+    desc = 'Definition',
+    icon = { icon = '', color = 'green' },
+    mode = 'n',
+  },
+  {
+    '<leader>gD',
+    '<cmd>lua vim.lsp.buf.declaration()<cr>',
+    desc = 'Declaration',
+    icon = { icon = '', color = 'cyan' },
+    mode = 'n',
+  },
 })
-
 -- Hover
 which_key.add({
-  { '<leader>s', group = 'Symbol' },
-  { '<leader>sa', '<cmd>lua vim.lsp.buf.code_action()<cr>', desc = 'Actions', mode = 'n' },
-  { '<leader>sr', '<cmd>lua vim.lsp.buf.rename()<cr>', desc = 'Rename', mode = 'n' },
-  { '<leader>ss', '<cmd>lua vim.lsp.buf.hover()<cr>', desc = 'Symbol', mode = 'n' },
-  { '<leader>sd', '<cmd>lua vim.diagnostic.open_float()<cr>', desc = 'Diagnostics', mode = 'n' },
+  { '<leader>s', group = 'Symbol', icon = { icon = '', color = 'blue' } },
+  {
+    '<leader>sa',
+    '<cmd>lua vim.lsp.buf.code_action()<cr>',
+    desc = 'Actions',
+    icon = { icon = '', color = 'red' },
+    mode = 'n',
+  },
+  {
+    '<leader>sr',
+    '<cmd>lua vim.lsp.buf.rename()<cr>',
+    desc = 'Rename',
+    icon = { icon = '', color = 'purple' },
+    mode = 'n',
+  },
+  {
+    '<leader>si',
+    '<cmd>lua vim.lsp.buf.hover()<cr>',
+    desc = 'Information',
+    icon = { icon = '', color = 'green' },
+    mode = 'n',
+  },
+  {
+    '<leader>sd',
+    '<cmd>lua vim.diagnostic.open_float()<cr>',
+    desc = 'Diagnostics',
+    icon = { icon = '', color = 'blue' },
+    mode = 'n',
+  },
 })
-
 -- Terminal
 which_key.add({
-  { '<leader>t', group = 'Floaterm' },
-  { '<leader>tt', '<cmd>FloatermToggle<cr>', desc = 'Toggle', mode = 'n' },
+  { '<leader>t', group = 'Floaterm', icon = { icon = '', color = 'green' } },
+  {
+    '<leader>tt',
+    '<cmd>FloatermToggle<cr>',
+    desc = 'Toggle',
+    icon = { icon = '', color = 'green' },
+    mode = 'n',
+  },
 })
-
 -- NvimTree
 which_key.add({
-  { '<leader>n', group = 'NvimTree' },
-  { '<leader>nn', '<cmd>NvimTreeToggle<cr>', desc = 'Toggle', mode = 'n' },
+  { '<leader>n', group = 'NvimTree', icon = { icon = '', color = 'red' } },
+  {
+    '<leader>nn',
+    '<cmd>NvimTreeToggle<cr>',
+    desc = 'Toggle',
+    icon = { icon = '', color = 'red' },
+    mode = 'n',
+  },
 })
