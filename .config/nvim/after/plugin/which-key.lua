7b695e33cf476bf390c0299235ded3f95ffe4487
Gabriel Moreno <gantoreno@gmail.com>
2024-10-21 17:20:49 -0400

    Update which-key configuration

--- .config/nvim/after/plugin/which-key.lua
+++ .config/nvim/after/plugin/which-key.lua
@@ -1,3 +1,11 @@
 require('which-key').setup({
-  preset = 'modern',
+  win = {
+    width = 0.999,
+    height = { min = 4, max = 25 },
+    col = 0.5,
+    row = -1,
+    border = 'rounded',
+    title = true,
+    title_pos = 'center',
+  },
 })
