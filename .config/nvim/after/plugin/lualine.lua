211f177fa6c3650985bb82f56f1d72862ddd90f4
Gabriel Moreno <gantoreno@gmail.com>
2024-10-23 12:37:18 -0400

    Remove globalstatus

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -40,6 +40,5 @@ require('lualine').setup({
   options = {
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
-    globalstatus = false,
   },
 })
