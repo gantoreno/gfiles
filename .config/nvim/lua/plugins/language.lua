b5603a3fa16933723c5ce53e313a975a83bc3673
Gabriel Moreno <gantoreno@gmail.com>
2024-10-26 15:45:48 -0400

    Fix treesitter highlighting

--- .config/nvim/lua/plugins/language.lua
+++ .config/nvim/lua/plugins/language.lua
@@ -270,6 +270,8 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = function()
       require('nvim-treesitter.install').update({ with_sync = true })
+    end,
+    config = function()
       require('nvim-treesitter.configs').setup({
         ensure_installed = 'all',
         autopairs = { enable = true },
