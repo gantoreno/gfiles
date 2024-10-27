f541ae35a115ac56d79d4eb200b6021788a8c7af
Gabriel Moreno <gantoreno@gmail.com>
2024-10-26 21:37:00 -0400

    Update git

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,6 +1,21 @@
 return {
   {
     'lewis6991/gitsigns.nvim',
-    opts = {},
+    opts = {
+      signs = {
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '▸' },
+        topdelete = { text = '▸' },
+        changedelete = { text = '▸' },
+        untracked = { text = '┃' },
+      },
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        delay = 500,
+        ignore_whitespace = false,
+      },
+    },
   },
 }
