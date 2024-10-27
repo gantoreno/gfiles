634a74198eec09df8c3ab2529aa54d582cf61cb9
Gabriel Moreno <gantoreno@gmail.com>
2024-10-26 21:36:08 -0400

    Update ui

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,15 +1,6 @@
 return {
   {
     'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '┃' },
-        change = { text = '┃' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-        untracked = { text = '┆' },
-      },
-    },
+    opts = {},
   },
 }
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,10 +1,24 @@
 return {
   {
+    'stevearc/dressing.nvim',
+    opts = {},
+  },
+  {
     'folke/noice.nvim',
     event = 'VeryLazy',
     opts = {
       presets = {
         command_palette = true,
+        lsp_doc_border = true,
+      },
+      lsp = {
+        hover = {
+          opts = {
+            border = {
+              padding = { 0, 0 },
+            },
+          },
+        },
       },
     },
     dependencies = {
