f84d58e6fe5f1987740bce32901a1b0a1e206e19
Gabriel Moreno <gantoreno@gmail.com>
2024-12-28 16:13:57 -0400

    Update config

--- .config/nvim/lua/plugins/tabline.lua
+++ .config/nvim/lua/plugins/tabline.lua
@@ -1,3 +1,7 @@
+local editor = require('earthbound.palettes.earthbound').editor
+local syntax = require('earthbound.palettes.earthbound').syntax
+local colors = require('earthbound.palettes.earthbound').colors
+
 return {
   {
     'akinsho/bufferline.nvim',
@@ -26,10 +30,52 @@ return {
         },
         highlights = {
           fill = {
-            link = 'NvimTreeNormal',
+            bg = editor.highlight,
+          },
+          background = {
+            bg = editor.highlight,
+          },
+          separator = {
+            fg = editor.highlight,
+            bg = editor.highlight,
+          },
+          separator_visible = {
+            fg = editor.highlight,
+            bg = editor.highlight,
+          },
+          separator_selected = {
+            fg = editor.highlight,
+            bg = editor.highlight,
+          },
+          close_button = {
+            fg = syntax.disabled,
+            bg = editor.highlight,
+          },
+          close_button_visible = {
+            fg = syntax.disabled,
+            bg = editor.highlight,
+          },
+          close_button_selected = {
+            fg = colors.red,
+          },
+          indicator_visible = {
+            fg = editor.highlight,
+          },
+          indicator_selected = {
+            fg = editor.background,
+          },
+          modified = {
+            fg = colors.green,
+          },
+          modified_visible = {
+            fg = colors.green,
+          },
+          modified_selected = {
+            fg = colors.green,
           },
           offset_separator = {
-            link = 'NvimTreeWinSeparator',
+            fg = editor.sidebar,
+            bg = editor.sidebar,
           },
         },
       })
